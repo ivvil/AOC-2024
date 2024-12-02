9a741a4e8cbe7987529540236be9ac12260fb839
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-12-02 13:31:01 +0100

    Add defday macro

--- aoc.lisp
+++ aoc.lisp
@@ -12,6 +12,19 @@
 
 (defgeneric get-input (day))
 
-;; (defmethod get-input ((day day))
-;;   )
+(defmethod get-input ((day day))
+  (let* ((class-name (symbol-name (class-name (class-of day)))) ; Get class name as string
+         (day-number (subseq class-name 3)) ; Extract the number after "day"
+         (path (merge-pathnames (format nil "~A/input" day-number) +input-path+)))
+    (unless (probe-file path)
+      (error "Input file ~A not found." path))
+	(format t "Class name: ~a. Day number: ~a. Path: ~a" class-name day-number path)
+    (uiop:read-file-string path)))
+
+(defmacro defday (day-number)
+  `(progn
+     (defclass ,(intern (format nil "DAY~A" day-number)) (day) ())
+     (pushnew ',(intern (format nil "DAY~A" day-number)) *days*)))
+
+
 
--- days/day1.lisp
+++ days/day1.lisp
@@ -2,7 +2,7 @@
 
 (in-package #:dev.shft.aoc-2024)
 
-(defclass day1 (day) ())
+(defday 1)
 
 (defmethod run ((day day1))
   (multiple-value-bind (left right) (get-input day)
@@ -11,15 +11,15 @@
     (apply #'+ (mapcar (lambda (x y) (abs (- x y))) left right))))
 
 (defmethod get-input ((day day1))
-  (let* ((path (merge-pathnames "1/input" +input-path+))
-         (file (uiop:read-file-lines path))
-         (left '())
-         (right '()))
-    (dolist (str file)
-      (when (and (stringp str) (> (length str) 0)) ; Validate line
-        (let ((split (split-sequence:split-sequence #\Space str)))
-          (when (>= (length split) 4) ; Ensure at least 4 elements
-            (push (parse-integer (first split) :junk-allowed t) left)
-            (push (parse-integer (fourth split) :junk-allowed t) right)))))
-    (values (sort left #'<) (sort right #'<))))
+  (let ((file (split-sequence:split-sequence #\Newline (call-next-method)))) ; Calls the generic method
+    (let ((left '())
+          (right '()))
+      (dolist (str file)
+        (when (and (stringp str) (> (length str) 0))
+          (let ((split (split-sequence:split-sequence #\Space str)))
+            (when (>= (length split) 4)
+              (push (parse-integer (first split) :junk-allowed t) left)
+              (push (parse-integer (fourth split) :junk-allowed t) right)))))
+      (values (sort left #'<) (sort right #'<)))))
+
 
