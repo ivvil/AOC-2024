725ccb6d85f3705a33d9df5a8e01a82eb29e358e
Iván Villagrasa <ivvil412@gmail.com>
2024-12-02 23:40:33 +0100

    First attempt for day2 part two

--- days/day2.lisp
+++ days/day2.lisp
@@ -12,12 +12,25 @@
 		(safe (mapcar #'day2-parse-line input)))
 	(count t safe)))
 
+(defmethod run-second ((day day2))
+  (let* ((input (get-input day))
+		 (safe (mapcar #'day2-parse-line-errors input)))
+	(count t safe)))
+
 (defun largest-distance (numbers)
   "Returns the largest distance between two numbers in the list"
   (reduce (lambda (max-dist pair)
 			(max max-dist (abs (- (second pair) (first pair)))))
 		  (mapcar #'list numbers (cdr numbers))
 		  :initial-value 0))
+
+(defun distance (a b)
+  (abs (- a b)))
+
+(defun compute-distance (numbers)
+  (mapcar (lambda (pair)
+			(distance (first pair) (second pair)))
+		  (mapcar #'list numbers (cdr numbers))))
 
 (defun trend (a b)
   "Gets the trend between two numbers"
@@ -30,10 +43,19 @@
 (defun compute-trends (numbers)
   "Compute the trend (:inc or :dec) between each consecutive pair of numbers in the list."
   (when (and numbers (cdr numbers)) ; Ensure the list has at least two elements
-    (mapcar #'(lambda (pair)
+    (mapcar (lambda (pair)
                 (trend (first pair) (second pair)))
             (mapcar #'list numbers (cdr numbers)))))
 
 (defun day2-parse-line (line)
   (let ((parts (mapcar #'parse-integer (split-sequence:split-sequence #\Space line))))
 	(and (> 4 (largest-distance parts)) (= 1 (list-length (remove-duplicates (compute-trends parts)))))))
+
+(defun day2-parse-line-errors (line)
+  (let* ((parts (split-sequence:split-sequence #\Space line))
+		(nums (mapcar #'parse-integer parts))
+		 (distance-errors (length (remove-if-not (lambda (x)
+										  (< 3 x))
+										(compute-distance nums))))
+		 (trend-errors (length (remove-duplicates (compute-trends nums)))))
+	(< 1 (+ distance-errors trend-errors))))
